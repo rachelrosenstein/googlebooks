1e6d555d67750a6f25d8c3efd603128628a688cd
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2021-02-04 09:06:24 -0500

    first attempt to heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const apiRoutes = require("./routes/index");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,12 +14,23 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use('/api', apiRoutes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/google-book-s',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
